6629b341a9a9e1a44aa4d81a507796f87fdb44c5
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-14 10:29:39 +0100

    basic tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(true)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    tracing::info!("Starting up");
+    tracing::warn!("Are you sure?");
+    tracing::error!("This is an error!")
 }
