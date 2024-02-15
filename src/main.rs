d1ffd79c29e191129b77c388f66d4e195bfe9ab0
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-15 08:53:04 +0100

    final tracing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-use tracing_subscriber::{fmt, Registry};
-use tracing_subscriber::{prelude::*, EnvFilter};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
-    let fmt_layer = fmt::layer()
-        .with_file(true)
-        .with_line_number(true)
-        .with_thread_ids(true)
-        .with_target(true);
-
-    let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("debug"))
-        .unwrap();
-
-    let subscriber = Registry::default().with(fmt_layer).with(filter_layer);
-
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-
-    tracing::info!("Starting up");
-    tracing::warn!("Are you sure?");
-    tracing::error!("This is an error!")
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or("error".into()))
+        .init();
 }
