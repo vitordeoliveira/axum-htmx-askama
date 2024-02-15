36349c8581557a61a9da5a85f2a1254ff775c578
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-15 08:45:38 +0100

    layered solution

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use tracing_subscriber::{fmt, Registry};
+use tracing_subscriber::{prelude::*, EnvFilter};
+
 #[tokio::main]
 async fn main() {
-    let subscriber = tracing_subscriber::fmt()
-        .compact()
+    let fmt_layer = fmt::layer()
         .with_file(true)
         .with_line_number(true)
         .with_thread_ids(true)
-        .with_target(true)
-        .finish();
+        .with_target(true);
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("debug"))
+        .unwrap();
+
+    let subscriber = Registry::default().with(fmt_layer).with(filter_layer);
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
