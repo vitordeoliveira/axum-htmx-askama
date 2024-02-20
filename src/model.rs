3f6b7e1436f9ae24226a8d3d5b2ab11dbe6dce49
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-20 08:19:42 +0100

    basic model creation

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,14 @@
+use std::sync::{Arc, Mutex};
+
+use serde::Deserialize;
+
 #[derive(Debug)]
 struct AppState {
     todos: Mutex<Vec<Option<Todo>>>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
-struct Todo {
+pub struct Todo {
     id: u16,
     value: String,
     active: bool,
@@ -16,9 +20,26 @@ struct Todo {
 //     }
 // }
 
+// constructor
+impl ModelController {
+    pub async fn new() -> Result<Self, ()> {
+        Ok(Self {
+            todos_store: Arc::default(),
+        })
+    }
+}
+
 // Model Controller
 // Clone here just clone the ARC not the Vector
 #[derive(Clone)]
 pub struct ModelController {
     todos_store: Arc<Mutex<Vec<Option<Todo>>>>,
+}
+
+impl ModelController {
+    pub async fn get_todos(&self) -> Result<Vec<Todo>, ()> {
+        let store = self.todos_store.lock().unwrap();
+        let todos = store.iter().filter_map(|i| i.clone()).collect();
+        Ok(todos)
+    }
 }
